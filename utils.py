76abad522570211789a7f5beda7ac5fc6baf478f
Amine <mohammed.sahraoui@mail.mcgill.ca>
2024-01-13 17:53:06 -0500

    finished pre-processing

--- utils.py
+++ utils.py
@@ -1,18 +1,22 @@
-import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 
-class color_index_adder(BaseEstimator, TransformerMixin):
-    def __init__(self, filters = ['u', 'g', 'r', 'i', 'z']):
-        self.filters = filters
-    
+class ColorIndicesAdder(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self  
 
-    def transform(self, df, y=None):
-        galaxy_filer = df['u'] - df['g'] 
-        star_fiter = df['g'] - df['r']
-        quasar_filter_1 = df['g'] - df['r']
-        quasar_filer_2 = df['u'] - df['g'] 
+    def transform(self, X, y=None):
+        X['galaxy_filter'] = X['u'] - X['g']
+        X['star_filter'] = X['g'] - X['r']
+        X['quasar_filter_1'] = X['g'] - X['r']
+        X['quasar_filter_2'] = X['u'] - X['g']
+        return X
+    
+class ToDataFrame(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
 
-        # Ensure the output is a 2D array
-        return np.c_[df, galaxy_filer, star_fiter, quasar_filter_1, quasar_filer_2].reshape(-1, 1)
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return pd.DataFrame(X, columns=self.columns)
